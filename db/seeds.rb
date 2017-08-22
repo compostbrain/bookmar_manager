86d31c226620f85f090b905c5ba21447a6963e9c
compostbrain <joel@wildmeadowsfarm.com>
2017-08-22 09:40:57 -0400

    Added topic partial and bookmark card

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,11 @@
 50.times do
  Topic.create!(title: Faker::Lorem.sentence, user_id: @user.id)
 end
+topics = Topic.all
+300.times do
+  Bookmark.create!(url: Faker::Internet.url, topic: topics.sample)
+end
 
 puts "1 user created"
 puts "50 topics created."
+puts "300 bookmarks created."
--- spec/features/topics_spec.rb
+++ spec/features/topics_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Navigatiion' do
+describe 'Navigation' do
   let :factory_user { create(:user) }
   before do
     sign_in factory_user
@@ -10,8 +10,8 @@ describe 'Navigatiion' do
     before do
       @topic1 = FactoryGirl.create(:topic, user: factory_user)
       @topic2 = FactoryGirl.create(:topic, user: factory_user)
-      bookmark1 = FactoryGirl.create(:bookmark, topic: @topic1)
-      bookmark2 = FactoryGirl.create(:bookmark, topic: @topic2)
+      @bookmark1 = FactoryGirl.create(:bookmark, topic: @topic1)
+      @bookmark2 = FactoryGirl.create(:bookmark, topic: @topic2)
       visit topics_path
     end
 
@@ -20,12 +20,13 @@ describe 'Navigatiion' do
     end
 
     it 'has a title of Topics' do
-      expect(page).to have_content(/Topics#index/)
+      expect(page).to have_content(/Topics/)
     end
 
     xit 'has a list of Topics with associated bookmarks' do
       within(:css, "#{@topic1.slug}") do
-        find
+
+        expect(page.find("#bookmark").all).to eq(1)
         # find all bookmarks and expect the size to be 1
       end
     end

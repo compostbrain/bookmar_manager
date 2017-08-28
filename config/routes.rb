72d43bba325837e0c532623e03517893b1a8c53b
compostbrain <joel@wildmeadowsfarm.com>
2017-08-28 14:15:40 -0400

    added mailgun vars and created incoming controller to handle incoming emails

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'dashboard#index'
+  post :incoming, to: 'incoming#create'
   resources :topics do
     resources :bookmarks, except: [:index]
   end

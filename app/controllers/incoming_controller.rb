74428d5d3bf3a9e5cde8060bcb6797a249644ec6
compostbrain <joel@wildmeadowsfarm.com>
2017-08-30 07:59:34 -0400

    added logic to show method in incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,27 +8,27 @@ class IncomingController < ApplicationController
     # to get a sense of what you're dealing with.
     puts "INCOMING PARAMS HERE: #{params}"
 
-    #  incoming_user = params[:sender]
-    #  incoming_topic = params[:subject]
-    #  incoming_bookmark = params["body-plain"]
+    incoming_user = params[:sender]
+    incoming_topic = params[:subject]
+    incoming_bookmark = params["body-plain"]
 
-    #  if !User.exists?(email: incoming_user)
-    #    new_user = User.create(email: incoming_user, password: "password", username: incoming_user)
-    #  else
-    #    new_user = User.find(params[])
-    #  end
-     #
-    #  if !Topic.exists?(title: incoming_topic)
-    #    new_topic = Topic.create(title: incoming_topic, user_id: new_user)
-    #  else
-    #    new_topic = incoming_topic
-    #  end
-     #
-     #
-    #  if !Bookmark.exist?(url: incoming_bookmark, topic_id: new_topic)
-    #    new_bookmark = Bookmark.create(url: incoming_bookmark, topic_id: new_topic)
-     #
-    #  end
+    if !User.exists?(email: incoming_user)
+      new_user = User.create(email: incoming_user, password: "password", username: incoming_user)
+    else
+      new_user = User.find(incoming_user)
+    end
+
+    if !Topic.exists?(title: incoming_topic)
+      new_topic = Topic.create(title: incoming_topic, user_id: new_user)
+    else
+      new_topic = Topic.find(incoming_topic)
+    end
+
+
+    if !Bookmark.exist?(url: incoming_bookmark, topic_id: new_topic)
+      new_bookmark = Bookmark.create(url: incoming_bookmark, topic_id: new_topic)
+
+    end
 
 
      # Now that you're sure you have a valid user and topic, build and save a new bookmark

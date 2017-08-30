ead4ff50898101658feab8a32d4899c293933598
compostbrain <joel@wildmeadowsfarm.com>
2017-08-30 15:23:30 -0400

    added users partials

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,8 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :topic
   has_many :likes, dependent: :destroy
 
-  scope :liked_by, ->(user) { where(user_id: user.id) }
-
   def user
     topic.user
   end

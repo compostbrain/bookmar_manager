5496655e4dc90fbcb5ee063b7e51d58622c00ec7
compostbrain <joel@wildmeadowsfarm.com>
2017-08-30 15:39:28 -0400

    added only: [:create]

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,8 +1,8 @@
 class IncomingController < ApplicationController
 
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
-  skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!,
+  skip_before_action :verify_authenticity_token,, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
 
   def create
 
